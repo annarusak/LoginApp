db6ac9e25b36260ebea85d8b2b3f68d6b4d07db7
annarusak <alitvinetz@gmail.com>
2023-11-27 22:22:16 +0200

    Implement CustomButton, CustomTextField and Header classes. Add LoginViewController.

---
+++ LoginApp/Controllers/LoginViewController.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+class LoginController: UIViewController {
+
+    // MARK: - UI Components
+    private let headerView = HeaderView(title: "Sign In", subtitle: "Sign In to your account")
+    
+    private let emailField = CustomTextField(fieldType: .email)
+    private let passwordField = CustomTextField(fieldType: .password)
+    
+    private let signInButton = CustomButton(title: "Sign In", hasBackground: true, fontSize: .big)
+    private let newUserButton = CustomButton(title: "Create account", fontSize: .medium)
+    private let forgotPasswordButton = CustomButton(title: "Forgot password?", fontSize: .tiny)
+    
+    let backgroundColor = UIColor(red: 25/255, green: 30/255, blue: 49/255, alpha: 1)
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+        self.signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        self.newUserButton.addTarget(self, action: #selector(didTapNewUser), for: .touchUpInside)
+        self.forgotPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.view.backgroundColor = backgroundColor
+        
+        view.addSubview(headerView)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(signInButton)
+        view.addSubview(newUserButton)
+        view.addSubview(forgotPasswordButton)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        newUserButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 222),
+            
+            emailField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 20),
+            emailField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            emailField.heightAnchor.constraint(equalToConstant: 55),
+            emailField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 20),
+            passwordField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            passwordField.heightAnchor.constraint(equalToConstant: 55),
+            passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            forgotPasswordButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 4),
+            forgotPasswordButton.leftAnchor.constraint(equalTo: passwordField.leftAnchor),
+            forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            signInButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 65),
+            signInButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            signInButton.heightAnchor.constraint(equalToConstant: 55),
+            signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            newUserButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 20),
+            newUserButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            newUserButton.heightAnchor.constraint(equalToConstant: 44),
+            newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85)
+        ])
+    }
+    
+    // MARK: - Selectors
+    @objc private func didTapSignIn() {
+        print("signInTapped")
+    }
+    
+    @objc private func didTapNewUser() {
+        print("newUserTapped")
+    }
+    
+    @objc private func didTapForgotPassword() {
+        print("forgotPasswordTapped")
+    }
+
+
+}
